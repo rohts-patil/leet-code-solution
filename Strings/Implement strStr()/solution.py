9db7b95a38abaec689c6241a3f39ba1a85d3dde8
Rohit Patil <rohts.patil@gmail.com>
2019-01-31 00:46:25 +0530

    solution for strStr()

---
+++ Strings/Implement strStr()/solution.py
@@ -0,0 +1,14 @@
+#  https://leetcode.com/explore/featured/card/top-interview-questions-easy/127/strings/885/
+
+
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        for i in range(len(haystack) - len(needle)+1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1

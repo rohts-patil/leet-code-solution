e4df45104ebbd1d99c1e9b25b362fceaf299b255
Rohit Patil <rohts.patil@gmail.com>
2019-02-01 01:56:32 +0530

    solution for count and say

---
+++ Strings/Count and Say/solution.py
@@ -0,0 +1,14 @@
+#  https://leetcode.com/explore/featured/card/top-interview-questions-easy/127/strings/886/
+
+
+class Solution:
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        s = '1'
+        for _ in range(n - 1):
+            s = re.sub(
+                r'(.)\1*', lambda m: str(len(m.group(0))) + m.group(1), s)
+        return s

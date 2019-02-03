7d15cabc043250e210e9ec7a1da78d3b4b418d76
Rohit Patil <rohts.patil@gmail.com>
2019-02-03 22:13:59 +0530

    solution for longest common prefix

---
+++ Strings/Longest Common Prefix/solution.py
@@ -0,0 +1,17 @@
+#  https://leetcode.com/explore/featured/card/top-interview-questions-easy/127/strings/887/
+
+
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        shortest = min(strs,key=len)
+        for i, ch in enumerate(shortest):
+            for other in strs:
+                if other[i] != ch:
+                    return shortest[:i]
+        return shortest

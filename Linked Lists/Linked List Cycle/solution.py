d14070bb1e451767b053d996b00d601643529b41
Rohit Patil <rohts.patil@gmail.com>
2019-02-07 23:42:33 +0530

    solution for linked list cycle

---
+++ Linked Lists/Linked List Cycle/solution.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/explore/featured/card/top-interview-questions-easy/93/linked-list/773/
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        try:
+            slow = head
+            fast = head.next
+            while slow is not fast:
+                slow = slow.next
+                fast = fast.next.next
+            return True
+        except:
+            return False

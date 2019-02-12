9992a52eeffe62f4ecc3dfd687cd7b7d980f5a2c
Rohit Patil <rohts.patil@gmail.com>
2019-02-12 21:15:50 +0530

    solution for count primes

---
+++ Math/Count Primes/solution.py
@@ -0,0 +1,19 @@
+#  https://leetcode.com/explore/featured/card/top-interview-questions-easy/102/math/744/
+
+
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 2:
+            return 0
+        res = [True] * n
+        res[0] = res[1] = False
+        for i in range(2, n):
+            if res[i] == True:
+                for j in range(2, (n-1)//i+1):
+                    res[i*j] = False
+        return sum(res)
+                

afd1cf2f94a1dc4c1f38bfb7c4453d1256caff12
Rohit Patil <rohts.patil@gmail.com>
2020-04-19 12:11:07 +0530

    added solution for mim path sum

---
+++ 30-day-leetcoding-challenge/Minimum Path Sum/Solution.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/530/week-3/3303/
+
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[] dp = new int[n];
+        dp[0] = grid[0][0];
+
+        // set the first row
+        for (int i = 1; i < n; i++) {
+            dp[i] = dp[i - 1] + grid[0][i];
+        }
+
+        for (int i = 1; i < m; i++) {
+            dp[0] = dp[0] + grid[i][0];
+            for (int j = 1; j < n; j++) {
+                dp[j] = Math.min(dp[j], dp[j - 1]) + grid[i][j];
+            }
+        }
+
+        return dp[n - 1];
+    }
+}

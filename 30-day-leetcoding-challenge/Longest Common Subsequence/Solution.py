c8ca790167ad75f89b0699ed8cd7a9b7758c0a11
Rohit Patil <rohts.patil@gmail.com>
2020-04-26 22:48:36 +0530

    added solution for longest common subsequence

---
+++ 30-day-leetcoding-challenge/Longest Common Subsequence/Solution.py
@@ -0,0 +1,18 @@
+// https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/531/week-4/3311/
+
+class Solution:
+    def longestCommonSubsequence(self, s1: str, s2: str) -> int:
+        m = len(s1)
+        n = len(s2)
+        if m < n:
+            return self.longestCommonSubsequence(s2, s1)
+        memo = [[0 for _ in range(n + 1)] for _ in range(2)]
+
+        for i in range(m):
+            for j in range(n):
+                if s1[i] == s2[j]:
+                    memo[1 - i % 2][j + 1] = 1 + memo[i % 2][j]
+                else:
+                    memo[1 - i % 2][j + 1] = max(memo[1 - i % 2][j], memo[i % 2][j + 1])
+
+        return memo[m % 2][n]

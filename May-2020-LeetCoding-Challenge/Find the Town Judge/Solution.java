a81d5f49dd08e23a97bf6a16261a7a7bda03d745
Rohit Patil <rohts.patil@gmail.com>
2020-05-11 01:07:36 +0530

    added solution for town judge

---
+++ May-2020-LeetCoding-Challenge/Find the Town Judge/Solution.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3325/
+
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+        int[] count = new int[N+1];
+        for (int[] t: trust) {
+            count[t[0]]--;
+            count[t[1]]++;
+        }
+        for (int i = 1; i <= N; ++i) {
+            if (count[i] == N - 1) return i;
+        }
+        return -1;
+    }
+}

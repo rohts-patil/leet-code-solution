bc01247e45a0f134f69ac3d3f06bdf8c0916df05
Rohit Patil <rohts.patil@gmail.com>
2020-05-20 10:24:43 +0530

    added solution for online stock span

---
+++ May-2020-LeetCoding-Challenge/Online Stock Span/Solution.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3334/
+
+class StockSpanner {
+    Stack<Integer> prices, weights;
+
+    public StockSpanner() {
+        prices = new Stack();
+        weights = new Stack();
+    }
+
+    public int next(int price) {
+        int w = 1;
+        while (!prices.isEmpty() && prices.peek() <= price) {
+            prices.pop();
+            w += weights.pop();
+        }
+
+        prices.push(price);
+        weights.push(w);
+        return w;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */

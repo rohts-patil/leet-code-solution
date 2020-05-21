26825bb41041204f393a6a919feae1e8722bba85
Rohit Patil <rohts.patil@gmail.com>
2020-05-21 20:16:45 +0530

    added solution for count square submatrices with all 1

---
+++ May-2020-LeetCoding-Challenge/Count Square Submatrices with All Ones/Solution.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3336/
+
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int res = 0;
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix[0].length; ++j) {
+                if (matrix[i][j] > 0 && i > 0 && j > 0) {
+                    matrix[i][j] = Math.min(matrix[i - 1][j - 1], Math.min(matrix[i - 1][j], matrix[i][j - 1])) + 1;
+                }
+                res += matrix[i][j];
+            }
+        }
+        return res;
+    }
+}

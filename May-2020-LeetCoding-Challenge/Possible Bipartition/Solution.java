ccfb33994fb5f94267833a1c47c0a0f0e5e40ad1
Rohit Patil <rohts.patil@gmail.com>
2020-05-27 23:45:38 +0530

    added solution for possible bipartition

---
+++ May-2020-LeetCoding-Challenge/Possible Bipartition/Solution.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/537/week-4-may-22nd-may-28th/3342/
+
+class Solution {
+    ArrayList<Integer>[] graph;
+    Map<Integer, Integer> color;
+
+    public boolean possibleBipartition(int N, int[][] dislikes) {
+        graph = new ArrayList[N+1];
+        for (int i = 1; i <= N; ++i)
+            graph[i] = new ArrayList();
+
+        for (int[] edge: dislikes) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+
+        color = new HashMap();
+        for (int node = 1; node <= N; ++node)
+            if (!color.containsKey(node) && !dfs(node, 0))
+                return false;
+        return true;
+    }
+
+    public boolean dfs(int node, int c) {
+        if (color.containsKey(node))
+            return color.get(node) == c;
+        color.put(node, c);
+
+        for (int nei: graph[node])
+            if (!dfs(nei, c ^ 1))
+                return false;
+        return true;
+    }
+}

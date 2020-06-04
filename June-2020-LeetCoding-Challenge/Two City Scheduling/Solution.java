6d5e25854ada6b08cb633adad2e88ceee8d6ede0
Rohit Patil <rohts.patil@gmail.com>
2020-06-04 11:09:49 +0530

    added solution for two city scheduling

---
+++ June-2020-LeetCoding-Challenge/Two City Scheduling/Solution.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3349/
+
+class Solution {
+     public int twoCitySchedCost(int[][] costs) {
+        int N = costs.length/2;
+        int[] refund = new int[N * 2];
+        int minCost = 0, index = 0;
+        for(int[] cost : costs){
+            refund[index++] = cost[1] - cost[0];
+            minCost += cost[0];
+        }
+        Arrays.sort(refund);
+        for(int i = 0; i < N; i++){
+            minCost += refund[i];
+        }
+        return minCost;
+    }
+}

39b2d3a0273521877779e7ce141f8292d65c2d87
Rohit Patil <rohts.patil@gmail.com>
2020-06-07 20:22:21 +0530

    added solution for coin change 2

---
+++ June-2020-LeetCoding-Challenge/Coin Change 2/Solution.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3353/
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
+}

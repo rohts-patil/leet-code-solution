8cfc1af168b2c942b0126030946cfe360cddadb0
Rohit Patil <rohts.patil@gmail.com>
2020-06-10 11:03:05 +0530

    added solution for is subsequence

---
+++ June-2020-LeetCoding-Challenge/Is Subsequence/Solution.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3355/
+
+public class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) return true;
+        int indexS = 0, indexT = 0;
+        while (indexT < t.length()) {
+            if (t.charAt(indexT) == s.charAt(indexS)) {
+                indexS++;
+                if (indexS == s.length()) return true;
+            }
+            indexT++;
+        }
+        return false;
+    }
+}

800549304a66ec32a00672d0dc7536f3136ee83a
Rohit Patil <rohts.patil@gmail.com>
2020-06-11 11:29:35 +0530

    added solution for search insert position

---
+++ June-2020-LeetCoding-Challenge/Search Insert Position/Solution.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3356/
+
+class Solution {
+    public int searchInsert(int[] A, int target) {
+        int low = 0, high = A.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(A[mid] == target) return mid;
+            else if(A[mid] > target) high = mid-1;
+            else low = mid+1;
+        }
+        return low;
+    }
+}

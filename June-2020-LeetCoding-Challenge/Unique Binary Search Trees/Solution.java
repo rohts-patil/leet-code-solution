356d97f99ed08361349d152603b5d6048e81dff5
Rohit Patil <rohts.patil@gmail.com>
2020-06-24 20:15:10 +0530

    added solution for unique bst

---
+++ June-2020-LeetCoding-Challenge/Unique Binary Search Trees/Solution.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/542/week-4-june-22nd-june-28th/3370/
+
+class Solution {
+    public int numTrees(int n) {
+  int [] G = new int[n+1];
+  G[0] = G[1] = 1;
+    
+  for(int i=2; i<=n; ++i) {
+    for(int j=1; j<=i; ++j) {
+      G[i] += G[j-1] * G[i-j];
+    }
+  }
+  return G[n];
+}
+}

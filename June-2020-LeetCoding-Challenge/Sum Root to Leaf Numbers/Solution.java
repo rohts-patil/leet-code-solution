84fc35f5fd8f518d1aa62a18a59ab1aea540b6fd
Rohit Patil <rohts.patil@gmail.com>
2020-06-26 20:08:49 +0530

    added solution for sum root to leaf numbers

---
+++ June-2020-LeetCoding-Challenge/Sum Root to Leaf Numbers/Solution.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/542/week-4-june-22nd-june-28th/3372/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   public int sumNumbers(TreeNode root) {
+	return sum(root, 0);
+}
+
+public int sum(TreeNode n, int s){
+	if (n == null) return 0;
+	if (n.right == null && n.left == null) return s*10 + n.val;
+	return sum(n.left, s*10 + n.val) + sum(n.right, s*10 + n.val);
+}
+}

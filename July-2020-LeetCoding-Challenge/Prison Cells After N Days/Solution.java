50c9ac8b87cb0bb51db8585b48bc4c8f3e4655b1
Rohit Patil <rohts.patil@gmail.com>
2020-07-03 16:35:49 +0530

    added solution for prison cells after N days

---
+++ July-2020-LeetCoding-Challenge/Prison Cells After N Days/Solution.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/explore/featured/card/july-leetcoding-challenge/544/week-1-july-1st-july-7th/3379/
+
+class Solution {
+    public int[] prisonAfterNDays(int[] cells, int N) {
+		if(cells==null || cells.length==0 || N<=0) return cells;
+        boolean hasCycle = false;
+        int cycle = 0;
+        HashSet<String> set = new HashSet<>(); 
+        for(int i=0;i<N;i++){
+            int[] next = nextDay(cells);
+            String key = Arrays.toString(next);
+            if(!set.contains(key)){
+                set.add(key);
+                cycle++;
+            }
+            else{
+                hasCycle = true;
+                break;
+            }
+            cells = next;
+        }
+        if(hasCycle){
+            N%=cycle;
+            for(int i=0;i<N;i++){
+                cells = nextDay(cells);
+            }   
+        }
+        return cells;
+    }
+    
+    private int[] nextDay(int[] cells){
+        int[] tmp = new int[cells.length];
+        for(int i=1;i<cells.length-1;i++){
+            tmp[i]=cells[i-1]==cells[i+1]?1:0;
+        }
+        return tmp;
+    }
+}

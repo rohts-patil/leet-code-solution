83b739e326e2003aa5548138461b39597b91ac63
Rohit Patil <rohts.patil@gmail.com>
2020-07-06 16:51:00 +0530

    added solution for plus one

---
+++ July-2020-LeetCoding-Challenge/Plus One/Solution.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/explore/featured/card/july-leetcoding-challenge/544/week-1-july-1st-july-7th/3382/
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        
+    int n = digits.length;
+    for(int i=n-1; i>=0; i--) {
+        if(digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        
+        digits[i] = 0;
+    }
+    
+    int[] newNumber = new int [n+1];
+    newNumber[0] = 1;
+    
+    return newNumber;
+}
+}

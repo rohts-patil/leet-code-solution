be6a96c98af9b3e4c0c3efd1c61ab1f6899099c4
Rohit Patil <rohts.patil@gmail.com>
2020-07-11 00:28:24 +0530

    added solution for flatten a multilevel doubly linked list

---
+++ July-2020-LeetCoding-Challenge/Flatten a Multilevel Doubly Linked List/Solution.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/545/week-2-july-8th-july-14th/3386/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        if( head == null) return head
+        Node p = head; 
+        while( p!= null) {
+            
+            if( p.child == null ) {
+                p = p.next;
+                continue;
+            }
+           
+            Node temp = p.child;
+           
+            while( temp.next != null ) 
+                temp = temp.next;
+            
+            temp.next = p.next;  
+            if( p.next != null )  p.next.prev = temp;
+            
+            p.next = p.child; 
+            p.child.prev = p;
+            p.child = null;
+        }
+        return head;
+    }
+}

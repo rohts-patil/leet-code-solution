be237ae4b8ae4aeece6844c347ae39ad8c27faac
Rohit Patil <rohts.patil@gmail.com>
2020-07-11 19:51:58 +0530

    added solution for subsets

---
+++ July-2020-LeetCoding-Challenge/Subsets/Solution.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/545/week-2-july-8th-july-14th/3387/
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, 0);
+    return list;
+}
+
+private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+    list.add(new ArrayList<>(tempList));
+    for(int i = start; i < nums.length; i++){
+        tempList.add(nums[i]);
+        backtrack(list, tempList, nums, i + 1);
+        tempList.remove(tempList.size() - 1);
+    }
+}
+}

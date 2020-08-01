bab054d5fdcd3f3f7e6905f6a0dfd32a9af8b816
Rohit Patil <rohts.patil@gmail.com>
2020-08-01 17:38:07 +0530

    added solution for climbing stairs

---
+++ July-2020-LeetCoding-Challenge/Climbing Stairs/Solution.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/548/week-5-july-29th-july-31st/3407/
+
+public class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+}
